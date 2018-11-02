17ceb8c7aab197c5948eb9d25201d9aa70e085d9
SchrodingersKitty <radium213@gmail.com>
2018-11-03 00:39:10 +0200

    refining the urltest a bit

--- urltest.py
+++ urltest.py
@@ -1,34 +1,52 @@
 import requests
 
-fail = 0
-bad = 0
-good = 0
+jpegs = []
+gifs = []
+pngs = []
+others = []
+fails = []
 with open('roster.ini', 'r') as f:
     for url in f:
         try:
             r = requests.head(url[:-1])
         except:
-            fail += 1
+            fails.append(url)
             continue
         if r.status_code != 200:
-            fail += 1
+            fails.append(url)
             continue
         #url works
         ctype = r.headers.get('content-type')
         if not ctype:
-            fail += 1
+            fails.append(url)
             continue
+        if ctype == 'image/jpeg':
+            jpegs.append(url)
+        if ctype == 'image/png':
+            pngs.append(url)
         if ctype == 'image/gif':
-            bad += 1
-            with open('gifs.ini', 'a') as g:
-                g.write(url)
-        elif ctype == 'image/jpeg':
-            good += 1
-            with open('jpegs.ini', 'a') as g:
-                g.write(url)
+            gifs.append(url)
         else:
-            bad += 1
-            with open('others.ini', 'a') as g:
-                g.write(url)
+            others.append(url)
 
-print('good: {} bad: {} fail: {}'.format(good, bad, fail))
+with open('jpegs.ini', 'w') as f:
+    for url in jpegs:
+        f.write(url)
+
+with open('gifs.ini', 'w') as f:
+    for url in gifs:
+        f.write(url)
+
+with open('pngs.ini', 'w') as f:
+    for url in pngs:
+        f.write(url)
+
+with open('others.ini', 'w') as f:
+    for url in others:
+        f.write(url)
+
+with open('fails.ini', 'w') as f:
+    for url in fails:
+        f.write(url)
+
+print('good: {} bad: {} fail: {}'.format(len(jpegs)+len(pngs), len(gifs)+len(others), len(fails)))
