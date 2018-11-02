664d4b7b726174feb163e7e6da28b14dbd38caf4
SchrodingersKitty <radium213@gmail.com>
2018-11-02 23:00:40 +0200

    --reset option

--- core.py
+++ core.py
@@ -1,4 +1,4 @@
-import configparser, discord
+import configparser, discord, os, sys
 from discord.ext.commands import Bot
 from crawler.crawler import Crawler
 from db.db import Database
@@ -12,11 +12,16 @@ crawler = Crawler(pi['cid'], pi['sec'], pi['user'], pi['pwd'], pi['uage'])
 topBf = crawler.spawnTop()
 roster = crawler.generateBoss(topBf)
 
-database = Database()
-database.initTables()
-
-for iid, ititle, iscore, url, topcomment in roster:
-    database.registerBoss((iid, ititle, iscore, url, topcomment))
+dbfile = 'bossfight.db'
+if '--reset' in sys.argv:
+    if os.path.exists(dbfile):
+        os.remove(dbfile)
+    database = Database(dbfile)
+    database.initTables()
+    for iid, ititle, iscore, url, topcomment in roster:
+        database.registerBoss((iid, ititle, iscore, url, topcomment))
+else:
+    database = Database(dbfile)
 
 # Client Initialisation
 bot = Bot('$')
