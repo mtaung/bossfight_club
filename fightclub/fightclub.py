ffc45a596d85107c84f5cd6f405b4531dd27792b
mtaung <myat@aung.uk>
2018-11-03 22:38:02 +0000

    Prototype formatting for cards

--- fightclub/fightclub.py
+++ fightclub/fightclub.py
@@ -15,9 +15,13 @@ class Fightclub:
         pull = self.database.randomBoss('1')
         pull = pull[0] #Query returns list, need to go a level deeper
 
-        embed = discord.Embed(title=f'{pull[1]}', description=f'Power Level: {pull[2]}')
-        imageurl = pull[3]
-        embed.set_image(url=imageurl)
-        
+        # Card Formatting
+        embed = discord.Embed(title=f'Power Level: {pull[2]}', colour=discord.Colour.gold())
+        embed.set_author(name=f'{pull[1]}', icon_url='https://pbs.twimg.com/media/DT3rkg0VAAAYAKR.jpg')
+        embed.set_image(url=pull[3])
+        embed.add_field(name='Attack Name', value='Attack Value', inline=True)
+        embed.add_field(name='Attack Name', value='Attack Value', inline=True)
+        embed.add_field(name='Attack Name', value='Attack Value', inline=True)
+        embed.add_field(name='Attack Name', value='Attack Value', inline=True)
         embed.set_footer(text=f'{pull[4]}')
         await ctx.bot.send_message(ctx.message.channel, embed=embed)
