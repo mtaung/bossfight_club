5253281b2c4066179e6eaafe029d6a137e65097a
SchrodingersKitty <radium213@gmail.com>
2018-11-06 20:24:24 +0200

    changed to a more correct orm approach

--- db/tables.py
+++ db/tables.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Date, Sequence, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -12,6 +13,7 @@ class User(Base):
     losses = Column(Integer, default=0)
     pulls = Column(Integer, default=0)
     last_pull_date = Column(Date)
+    roster = relationship("Roster", back_populates="user")
 
 class Card(Base):
     __tablename__ = 'cards'
@@ -20,12 +22,15 @@ class Card(Base):
     image = Column(String(300))
     score = Column(Integer)
     sid = Column(String(6), unique=True)
+    roster_entries = relationship("Roster", back_populates="card")
 
 class Roster(Base):
     __tablename__ = 'rosters'
     id = Column(Integer, Sequence('roster_id_sequence'), primary_key=True)
     user_id = Column(String(20), ForeignKey("users.id"), nullable=False)
+    user = relationship("User", back_populates="roster")
     card_id = Column(Integer, ForeignKey("cards.id"), nullable=False)
+    card = relationship("Card", back_populates="roster_entries")
     level = Column(Integer, default=0)
     score = Column(Integer, default=0)
     attack_0 = Column(String(30))
